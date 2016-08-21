b0037c14352e2c140fc1dab641dafcc8c7f0f0cf
micheal stroud <micheal.stroud@gmail.com>
2016-08-21 11:31:18 -0500

    got references mostly working on the front end

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,17 +2,6 @@ class AccountsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_account, only: [:show, :edit, :update, :destroy]
 
-  # GET /accounts
-  # GET /accounts.json
-  def index
-    @accounts = Account.all
-  end
-
-  # GET /accounts/1
-  # GET /accounts/1.json
-  def show
-  end
-
   # GET /accounts/new
   def new
     @account = Account.new
@@ -25,21 +14,6 @@ class AccountsController < ApplicationController
 
     respond_to do |format|
       format.js { render :accountable_change, s: @s, a: @a, p: @p }
-    end
-  end
-
-  def create_poly_account
-    @account = Account.new
-    @poly_accountable_type = params[:at]
-
-  end
-
-
-  # GET /accounts/1/edit
-  def edit
-    respond_to do |format|
-      format.html
-      format.js
     end
   end
 
@@ -56,30 +30,6 @@ class AccountsController < ApplicationController
         format.html { render :new }
         format.json { render json: @account.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /accounts/1
-  # PATCH/PUT /accounts/1.json
-  def update
-    respond_to do |format|
-      if @account.update(account_params)
-        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
-        format.json { render :show, status: :ok, location: @account }
-      else
-        format.html { render :edit }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /accounts/1
-  # DELETE /accounts/1.json
-  def destroy
-    @account.destroy
-    respond_to do |format|
-      format.html { redirect_to accounts_url, notice: 'Account was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,7 @@
 module AccountsHelper
+
+  def create_account_generic_form(f)
+    render partial: 'generic_form', locals: { f: f }
+  end
+
 end
