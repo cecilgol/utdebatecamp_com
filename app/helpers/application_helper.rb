b49cddb9d3d5c1172797ddc40f1d3180ee6e066c
cecilgol <micheal.stroud@gmail.com>
2017-01-11 17:17:07 -0600

    unread_blog_posts check current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,9 @@ module ApplicationHelper
   end
 
   def unread_blog_posts
-    c = NewsPost.where("created_at > ? and published = true", current_user.last_sign_in_at).count
-    c > 0 ? c : ""
+    if current_user
+      c = NewsPost.where("created_at > ? and published = true", current_user.last_sign_in_at).count
+      c > 0 ? c : ""
+    end
   end
 end
